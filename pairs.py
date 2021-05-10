59e752d86eb85f627cc3bae3876b58537ec630bc
Tom <tomc74@gmail.com>
2021-05-10 16:22:07 -0700

    pairs.py updated to use local ref

--- pairs.py
+++ pairs.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup
-import requests 
 import dataset as ds
 import math
 import trueskill as ts
+from icecream import ic
+import pickle
 
 # trying to refactor this nonsense so I can actually grasp what I'm doing.
 
@@ -45,16 +45,12 @@ db = ds.connect('sqlite:///NABCseed.db') # actual DB
 #db = ds.connect('sqlite:///:memory:') # memory temporary
 table = db.create_table('players', primary_id='acbl')
 eventCodes = [
-	  ('NAPA','PLAT','IMP','IMPS','MIXD','OPPR','OPEN','FAST','WMPR','WMEN','WMLM','SILV','SLVR'),
-      ('LMPR','LM','LMO','OPEN','FAST','WAGR'),
-	  ('LMPR','OPEN','NAIL','BLUE','WMLM','WMPR','PINK','WMEN','SRMX','SMIX',
-	   'SMXD','SRMX','SRPR','SSP','SSRP','SSR')
+	  ('IMPS','MIXD','NAPA','PLAT','SILODOR','SLVR','SMITH'),
+      ('LM','WERNHER','FAST','WAGR'),
+	  ('BLUE','NAIL','SMIX','SS','WHITEHEAD')
     ]
 
 # inital session to scan
-
-url1= 'http://live.acbl.org/event/NABC'
-#url2= '3/SSR/'
 
 mp = {}
 
@@ -169,39 +165,24 @@ def addToDB(allrows):
 
 def main():
 	for year in range(14,20):
+		ic(year)
 		for season in range(3):
 			for event in eventCodes[season]:
-                #if season==2: 
-				#years = range(13,19)
-                #else: 
-				#years = range(14,20)
-			
-			#for year in years: 
 				for i in range(6):
-					testurl = url1 + str(year)+ str(season+1) + '/' + str(event) + '/' + str(i+1) + '/recap'
-					print("loading...", testurl)
+					testurl = event+str(year)+str(season)+'-'+str(i)+'.pkl'
+					
+					
 					logger('Event',testurl)
-					r1 = requests.get(testurl)
-					soup = BeautifulSoup(r1.content, 'html.parser')
-
-					soup_rows = soup.find_all('tr')
-
-					rows = []
-					for tr in soup_rows:
-						td = tr.find_all('td')
-						if td:
-							row = [td[1].get('data-acbl'), td[1].text,
-							td[2].get('data-acbl'), td[2].text, (td[11].text if (td[11].text!='') else td[15].text)]
-					# 11 or 15 works too, usable for imps
-							if row[0]: 
-								rows.append(row)
-					print('---------------------------')
-					print(len(rows))
-					print('---------------------------')
-
+					
+					try:
+						rows = pickle.load(open('pkls/'+testurl,'rb'))
+					except FileNotFoundError:
+						ic('no file',testurl)
+						rows = []
+					
 
 					if rows: addToDB(rows)
-
+		ic(len(db['players']))
 			
 getMPs()
 main()
